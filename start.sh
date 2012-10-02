00ce14da40f7821f58644bdf03db1b105c5f7c21
Benjamin Halsted <benjamin_halsted@yahoo.com>
2012-10-02 11:07:20 -0700

    Start lager in the start script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-erl -sname data_broadcast -cookie data_broadcast -pa apps/*/ebin -pa apps/deps/*/ebin -boot start_sasl -s data_broadcast -config ./start.config -args_file start.args
+erl -sname data_broadcast -cookie data_broadcast -pa apps/*/ebin -pa apps/deps/*/ebin -boot start_sasl -s lager -s data_broadcast -config ./start.config -args_file start.args
 
