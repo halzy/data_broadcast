f410783ccd196327fa9bd70950010ad158a89354
Benjamin Halsted <benjamin_halsted@yahoo.com>
2012-11-29 14:45:12 -0800

    Updated start to better handle applications that have yet to be started

--- src/data_broadcast.erl
+++ src/data_broadcast.erl
@@ -59,11 +59,15 @@ start_listener([{listen, Listen}|Listeners]) ->
             [{port, WsPort}], [{dispatch, Dispatch}]),
   start_listener(Listeners).
 
-stop(_State) ->
-    ok.
-
-start() ->
-  application:start(lager),
-  application:start(ranch),
-  application:start(cowboy),
-  application:start(data_broadcast).
+start() -> start(?MODULE).
+start(App) ->
+    start_ok(App, application:start(App, permanent)).
+start_ok(_App, ok) -> ok;
+start_ok(_App, {error, {already_started, _App}}) -> ok;
+start_ok(App, {error, {not_started, Dep}}) -> 
+    ok = start(Dep),
+    start(App);
+start_ok(App, {error, Reason}) -> 
+    erlang:error({app_start_failed, App, Reason}).
+stop() -> 
+    application:stop(?MODULE).
