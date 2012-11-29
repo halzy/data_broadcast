218443f05a282803e1099eba6a319d95c9db626a
Benjamin Halsted <benjamin_halsted@yahoo.com>
2012-11-29 14:49:22 -0800

    Updated start to better handle applications that have yet to be started

--- src/data_broadcast.erl
+++ src/data_broadcast.erl
@@ -3,7 +3,7 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/0, start/2, stop/1]).
+-export([start/0, start/2, stop/0]).
 
 %% ===================================================================
 %% Application callbacks
